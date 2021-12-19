a068c36d719f67f7b71e0c8191d924e015721569
unknown <nfederico@live.com>
2021-12-19 13:26:26 -0300

    Primera entrega, proyecto complilando y titulo principal cambiado

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View,Text} from "react-native";
+export default function Homes () {    
+   
+      return (
+        <View >
+          <Text>  ¡Hola Coder House!
+          </Text>
+          
+        </View>
+      );
+   
+ 
+}
